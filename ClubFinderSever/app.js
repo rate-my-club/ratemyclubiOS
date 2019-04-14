d5996022cb9aaa00966b5a2304519a2a67142bae
Sunil Green <green380@purdue.edu>
2019-04-14 13:06:32 -0400

    Backend Working

--- ClubFinderSever/app.js
+++ ClubFinderSever/app.js
@@ -4,7 +4,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 var Club = require('./models/club');
-var router = express.router;
+var router = express.Router();
+
+//router.use(bodyParser.json());
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -26,6 +29,7 @@ app.get('/clubfinder/clubs', function(req,res) {
 
   console.log("whats up");
   Club.find({}, function(err, user) {
+    res.send(user);
     console.log(JSON.stringify(user));
   })
 })
@@ -34,3 +38,41 @@ app.get('/clubfinder/clubs', function(req,res) {
 
 app.listen(3000);
 console.log("Got to this point");
+
+
+
+
+
+// var MongoClient = require('mongodb').MongoClient;
+//
+// // Connect to the db
+// MongoClient.connect("mongodb://localhost:27017/clubfinder", function (err, db) {
+//
+//     db.collection('Persons', function (err, collection) {
+//
+//         collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
+//         collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
+//         collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
+//
+//
+//
+//         db.collection('Persons').count(function (err, count) {
+//             if (err) throw err;
+//
+//             console.log('Total Rows: ' + count);
+//         });
+//     });
+//
+// });
+
+
+
+// var url = "mongodb://127.0.0.1:27017";
+//
+// var MongoClient = require('mongodb').MongoClient;
+//
+// MongoClient.connect(url, function(err,db) {
+//   if (err) throw err;
+//   console.log("Conected to MongoDB!");
+//   db.close()
+// });
--- ClubFinderSever/models/club.js
+++ ClubFinderSever/models/club.js
@@ -4,17 +4,30 @@ mongoose.connect('mongodb://localhost:27017/clubfinder', { useNewUrlParser: true
 
 var db = mongoose.connection;
 
-let Schema = mongoose.Schema;
+//let Schema = mongoose.Schema;
 
 const clubSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  image_url: String,
-  avg_rating: Number,
-  reviews: Array,
-  ratings: Array,
-})
+  name: {
+    type: String,
+    index: true
+  },
+  description: {
+    type: String,
+  },
+  image_url: {
+    type: String,
+  },
+  avg_rating: {
+    type: Number,
+  },
+  reviews: {
+    type: Array,
+  },
+  ratings: {
+    type: Array,
+  }
+}, { collection: 'purdue_university' });
 
-const Club = module.exports = mongoose.model('Activity', clubSchema);
+const Club = module.exports = mongoose.model('Club', clubSchema);
 
 module.exports = Club;
